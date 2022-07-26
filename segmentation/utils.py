9b21d4473e7ceb96590f0ba3b303ee230955be0c
John Morris <jgmorris1@mac.com>
2022-07-26 19:59:57 -0400

    Added a remap race function to separate white, black, and neither white nor black.

--- segmentation/utils.py
+++ segmentation/utils.py
@@ -5,3 +5,7 @@ def categorize_age(year_of_birth):
     return pd.cut(year_of_birth.astype(int), [0, 1946, 1965, 1981, 1997, 2030],
                   right=False,
                   labels=['S', 'B', 'GX', 'M', 'GZ'])
+
+
+def remap_race(race):
+    return race.apply(lambda x: 'WH' if x == 'WH' else 'BH' if x == 'BH' else 'NWB')
