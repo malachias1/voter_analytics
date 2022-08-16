a0dbfd49470b0641d22b930d4867ab4b76652a29
John Morris <jgmorris1@mac.com>
2022-08-16 05:32:38 -0400

    Mostly knock on effects of other actions.

--- forensics/models.py
+++ forensics/models.py
@@ -1,23 +1,35 @@
 import pandas as pd
-from django.db import models
-from election_results.models import ContestCategory, ContestCategoryMap, Detail, OverUnderVote
 import re
-from collections import defaultdict
-from county_details.models import CountyDetails
-import plotly.graph_objects as go
-from county_map.models import CountyMap
 import json
+from collections import defaultdict
 import plotly.express as px
+from county.models import County, CountyMap
+from election_results.models import ContestCategoryMap, OverUnderVote
+from core.models import MapConfig
+from core.base_fig import BaseMap
 
 
-class SplitTicketAnalysis:
-    def __init__(self, election_date):
-        self.election_date = election_date
-        self.rep_choice_p = re.compile(r'[(]REP[)]|[(]DEM[)]')
-        self.is_rep_p = re.compile(r'[(]REP[)]')
-        self.is_dem_p = re.compile(r'[(]DEM[)]')
-        self.is_perdue_p = re.compile(r'PERDUE.*[(]REP[)]')
-        self.is_ossoff_p = re.compile(r'OSSOFF.*[(]DEM[)]')
+class SplitTicketMapConfig(MapConfig):
+    @property
+    def contest_patterns(self):
+        return self.config['contest_patterns']
+
+    @property
+    def undervote_category(self):
+        return self.config['undervote_category']
+
+    @property
+    def undervote_column_name(self):
+        return self.config['undervote_column_name']
+
+
+class SplitTicketAnalysis(BaseMap):
+    def __init__(self, config_path):
+        self.config = SplitTicketMapConfig(config_path)
+
+    @property
+    def election_date(self):
+        return self.config.date
 
     def count_votes(self, party_selector, category):
         mappings = ContestCategoryMap.objects.filter(detail__election_date=self.election_date,
@@ -41,99 +53,39 @@ class SplitTicketAnalysis:
 
         return undervote_count
 
-    def get_state_biden_splits(self):
-        us_senate_votes = self.count_votes(self.is_ossoff_p, 'us_senate')
-        us_house_votes = self.count_votes(self.is_dem_p, 'us_house')
-        ga_house_votes = self.count_votes(self.is_dem_p, 'ga_house')
-        ga_senate_votes = self.count_votes(self.is_dem_p, 'ga_senate')
-        trump_votes = self.count_votes(self.is_rep_p, 'president')
-        binden_votes = self.count_votes(self.is_dem_p, 'president')
-        president_undervote = self.count_undervotes('president')
+    def get_state_splits(self):
+        splits = {}
+        standards = []
+        for category, pattern, colname, is_standard in self.config.contest_patterns:
+            if is_standard:
+                standards.append(colname)
+            splits[colname] = self.count_votes(re.compile(pattern), category)
+        splits[self.config.undervote_column_name] = self.count_undervotes(self.config.undervote_category)
 
-        splits = [{'county_code': x,
-                   'us_senate_vote': us_senate_votes[x],
-                   'us_house_vote': us_house_votes[x],
-                   'ga_house_vote': ga_house_votes[x],
-                   'ga_senate_vote': ga_senate_votes[x],
-                   'trump_vote': trump_votes[x],
-                   'biden_vote': binden_votes[x],
-                   'president_undervote': president_undervote[x]
-                   } for x in us_house_votes.keys()]
-        splits = pd.DataFrame.from_records(splits)
-        county_details = CountyDetails.objects.details
-        max_votes = splits[['us_senate_vote', 'us_house_vote', 'ga_house_vote', 'ga_senate_vote']].max(axis=1)
-        splits = splits.merge(county_details, on=['county_code'], how='inner').drop(
-            columns=['county_fips'])
-        splits = splits.assign(max_votes=max_votes,
-                               max_diff=splits.biden_vote - max_votes,
-                               margin=splits.biden_vote * 100 / (splits.trump_vote + splits.biden_vote))
-        splits = splits.sort_values(by=['max_diff'], ascending=False).reset_index(drop=True)
-        splits = splits.assign(rank=range(1, len(splits.index) + 1))
-        return splits
+        county_codes = splits[list(splits.keys())[0]]
+        records = []
+        for cc in county_codes:
+            record = {'county_code': cc}
+            for key in splits.keys():
+                record[key] = splits[key][cc]
+            records.append(record)
+        df = pd.DataFrame.from_records(records)
+        county_details = County.objects.as_df
+        max_votes = df[standards].max(axis=1)
+        df = df.merge(county_details, on=['county_code'], how='inner').drop(columns=['county_fips'])
+        df = df.assign(max_votes=max_votes,
+                       max_diff=df.candidate1 - max_votes,
+                       margin=df.candidate1 * 100 / (df.candidate1 + df.candidate2))
+        df = df.sort_values(by=['max_diff'], ascending=False).reset_index(drop=True)
+        df = df.assign(rank=range(1, len(df.index) + 1))
+        return df
 
-    def get_state_trump_splits(self):
-        us_senate_votes = self.count_votes(self.is_perdue_p, 'us_senate')
-        us_house_votes = self.count_votes(self.is_rep_p, 'us_house')
-        ga_house_votes = self.count_votes(self.is_rep_p, 'ga_house')
-        ga_senate_votes = self.count_votes(self.is_rep_p, 'ga_senate')
-        trump_votes = self.count_votes(self.is_rep_p, 'president')
-        binden_votes = self.count_votes(self.is_dem_p, 'president')
-        president_undervote = self.count_undervotes('president')
-
-        splits = [{'county_code': x,
-                   'us_senate_vote': us_senate_votes[x],
-                   'us_house_vote': us_house_votes[x],
-                   'ga_house_vote': ga_house_votes[x],
-                   'ga_senate_vote': ga_senate_votes[x],
-                   'trump_vote': trump_votes[x],
-                   'biden_vote': binden_votes[x],
-                   'president_undervote': president_undervote[x]
-                   } for x in us_house_votes.keys()]
-        splits = pd.DataFrame.from_records(splits)
-        county_details = CountyDetails.objects.details
-        max_votes = splits[['us_senate_vote', 'us_house_vote', 'ga_house_vote', 'ga_senate_vote']].max(axis=1)
-        splits = splits.merge(county_details, on=['county_code'], how='inner').drop(
-            columns=['county_fips'])
-        splits = splits.assign(max_votes=max_votes,
-                               max_diff=splits.trump_vote - max_votes,
-                               margin=splits.trump_vote * 100 / (splits.trump_vote + splits.biden_vote))
-        splits = splits.sort_values(by=['max_diff'], ascending=False).reset_index(drop=True)
-        splits = splits.assign(rank=range(1, len(splits.index) + 1))
-        return splits
-
-    def get_state_trump_splits_map(self, splits):
+    def get_split_choroplath(self):
+        splits = self.get_state_splits()
         state_map = CountyMap.objects.state_map
-        hover_data = {
-            'county_name': True,
-            'max_diff': True,
-            'president_undervote': True,
-            'trump_vote': True,
-            'biden_vote': True,
-            'margin': ':.1f',
-            'us_senate_vote': True,
-            'us_house_vote': True,
-            'ga_house_vote': True,
-            'ga_senate_vote': True,
-            'max_votes': True,
-            'county_code': False,
-        }
-        labels = {
-            'county_name': 'County Name',
-            'max_diff': 'Trump Performance vs. Estimated Max (votes)',
-            'president_undervote': 'President Under Vote Count',
-            'trump_vote': 'Former President Trump Tally',
-            'biden_vote': 'President Biden Tally',
-            'margin': 'Trump Vote %',
-            'us_senate_vote': 'Aggregate US Senate (Perdue) Vote Tally',
-            'us_house_vote': 'Aggregate US House Vote Tally',
-            'ga_senate_vote': 'Aggregate State Senate Vote Tally',
-            'ga_house_vote': 'Aggregate State House Vote Tally',
-            'max_votes': 'Observed Max Republican Vote'
-        }
-
         state_map = state_map.merge(splits, on=['county_code', 'county_name'], how='inner')
         gj = json.loads(state_map.to_json())
-        center = state_map.to_crs(crs='epsg:3035').centroid.to_crs(crs='epsg:4326').iloc[0]
+        center = self.centroid(state_map).iloc[0]
 
         fig = px.choropleth_mapbox(
             state_map,
@@ -145,155 +97,12 @@ class SplitTicketAnalysis:
             opacity=0.5,
             mapbox_style="open-street-map",
             zoom=6.5,
-            labels=labels,
-            hover_data=hover_data,
+            labels=self.config.labels,
+            hover_data=self.config.hover_data,
             color_discrete_sequence=px.colors.sequential.Plasma
         )
 
-        fig.update_layout(
-            legend=dict(
-                yanchor="top",
-                y=0.99,
-                xanchor="left",
-                x=0.01,
-                traceorder="reversed",
-                title_font_family="Times New Roman",
-                title_font_color="black",
-                font=dict(
-                    family="Courier",
-                    size=12,
-                    color="black"
-                ),
-                bgcolor="white",
-                bordercolor="Black",
-                borderwidth=1
-            ),
-            annotations=[
-                go.layout.Annotation(
-                    text='Copyright November Pathways, 2022',
-                    align='center',
-                    showarrow=False,
-                    yanchor="middle",
-                    xanchor="center",
-                    borderwidth=0,
-                    font=dict(
-                        family="Times New Roman",
-                        size=48,
-                        color="gray"
-                    ),
-                    opacity=0.5)
-            ]
-        )
-
-        fig.update_layout(margin={"r": 0, "t": 30, "l": 0, "b": 30})
-        return fig
-
-    def get_state_trump_splits_map2(self, splits):
-        state_map = CountyMap.objects.state_map
-        hover_data = {
-            'county_name': True,
-            'rank': True,
-            'max_diff': True,
-            'president_undervote': True,
-            'trump_vote': True,
-            'biden_vote': True,
-            'margin_str': True,
-            'us_house_vote': True,
-            'ga_house_vote': True,
-            'ga_senate_vote': True,
-            'max_votes': True,
-            'county_code': False,
-            'margin': False
-        }
-        labels = {
-            'county_name': 'County Name',
-            'rank': 'Trump Performance Rank',
-            'max_diff': 'Trump Under/Over Performance (votes)',
-            'president_undervote': 'Under Vote Count',
-            'trump_vote': 'Former President Trump Tally',
-            'biden_vote': 'President Biden Tally',
-            'margin_str': 'Trump Vote Percentage',
-            'us_house_vote': 'Aggregate US House Vote Tally',
-            'ga_house_vote': 'Aggregate State House Vote Tally',
-            'ga_senate_vote': 'Aggregate State Senate Vote Tally',
-            'max_votes': 'Observed Max Republican Vote'
-        }
-        hovertemplate = """
-        <b>%{county_name} County</b><br>
-        <b>Trump Under/Over Performance (votes)</b>: %{max_diff}<br>
-        <i>Aggregate US House Vote Tally</i>: %{us_house_vote}<br>
-        <i>Aggregate State House Vote Tally</i>: %{ga_house_vote}<br>
-        <i>Aggregate State Senate Vote Tally</i>: %{ga_senate_vote}<br>
-        The observed max Republican vote is the 
-        maximum of the above three tallies.<br>
-        <i>Observed Max</i>: %{max_votes}<br>
-        <i>Under Vote Count</i>: %{president_undervote}<br>
-        <i>Trump Tally</i>: %{trump_vote}<br>
-        <i>Biden Tally</i>: %{biden_vote}<br>
-        <i>Trump Margin of Victory/Defeat</i>: %{margin}<br>
-        """
-
-        splits = splits.assign(margin=splits.trump_vote / (splits.trump_vote + splits.biden_vote))
-        splits = splits.assign(margin_str=splits.margin.apply(lambda x: f'{x * 100:.1f}%'))
-        # splits = splits.sort_values(by=['max_diff'], ascending=False).reset_index(drop=True)
-        # splits = splits.assign(rank=range(1, len(splits.index) + 1))
-
-        state_map = state_map.merge(splits, on='county_code', how='inner')
-        gj = json.loads(state_map.to_json())
-        center = state_map.to_crs(crs='epsg:3035').centroid.to_crs(crs='epsg:4326').iloc[0]
-
-        data = go.Choroplethmapbox(
-            geojson=gj,
-            # featureidkey="properties.county_code",
-            customdata=splits,
-            hovertemplate=hovertemplate,
-            # color_discrete_sequence=px.colors.sequential.Plasma,
-            autocolorscale=True,
-            colorscale="Viridis", zmin=0, zmax=12,
-            marker_opacity=0.5, marker_line_width=0,
-            z=splits.margin
-        )
-
-        fig = go.Figure(data=data)
-
-        fig.update_layout(mapbox_style="open-street-map",
-                          mapbox_zoom=6.5,
-                          mapbox_center={"lat": center.y, "lon": center.x})
-
-        fig.update_layout(
-            legend=dict(
-                yanchor="top",
-                y=0.99,
-                xanchor="left",
-                x=0.01,
-                traceorder="reversed",
-                title_font_family="Times New Roman",
-                title_font_color="black",
-                font=dict(
-                    family="Courier",
-                    size=12,
-                    color="black"
-                ),
-                bgcolor="white",
-                bordercolor="Black",
-                borderwidth=1
-            ),
-            annotations=[
-                go.layout.Annotation(
-                    text='Copyright November Pathways, 2022',
-                    align='center',
-                    showarrow=False,
-                    yanchor="middle",
-                    xanchor="center",
-                    borderwidth=0,
-                    font=dict(
-                        family="Times New Roman",
-                        size=48,
-                        color="gray"
-                    ),
-                    opacity=0.5)
-            ]
-        )
-
+        self.add_watermark(fig)
+        self.add_legend_text(fig, self.config.description)
         fig.update_layout(margin={"r": 0, "t": 30, "l": 0, "b": 30})
         return fig
