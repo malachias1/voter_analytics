697ca65e14f612c0bcd0b7da74141ed961adaa1f
John Morris <jgmorris1@mac.com>
2022-08-18 07:33:03 -0400

    Improved map.

--- forensics/models.py
+++ forensics/models.py
@@ -53,6 +53,13 @@ class SplitTicketAnalysis(BaseMap):
 
         return undervote_count
 
+    def format_max_diff(self, value):
+        if value < 0:
+            return f'<b>({value:,})</b>'
+        if value == 0:
+            return 0
+        return f'+{value:,}'
+
     def get_state_splits(self):
         splits = {}
         standards = []
@@ -76,8 +83,9 @@ class SplitTicketAnalysis(BaseMap):
         df = df.assign(max_votes=max_votes,
                        max_diff=df.candidate1 - max_votes,
                        margin=df.candidate1 * 100 / (df.candidate1 + df.candidate2))
-        df = df.sort_values(by=['max_diff'], ascending=False).reset_index(drop=True)
+        df = df.sort_values(by=['max_diff'], ascending=False)
         df = df.assign(rank=range(1, len(df.index) + 1))
+        df = df.assign(max_diff=(df.candidate1 - max_votes).apply(self.format_max_diff))
         return df
 
     def get_split_choroplath(self):
@@ -86,7 +94,9 @@ class SplitTicketAnalysis(BaseMap):
         state_map = state_map.merge(splits, on=['county_code', 'county_name'], how='inner')
         gj = json.loads(state_map.to_json())
         center = self.centroid(state_map).iloc[0]
-
+        customdata = state_map.drop(columns=['county_code', 'county_fips',
+                                             'geoid_x', 'geoid_y',
+                                             'geometry']).to_records(index=False)
         fig = px.choropleth_mapbox(
             state_map,
             geojson=gj,
@@ -96,13 +106,32 @@ class SplitTicketAnalysis(BaseMap):
             center={"lat": center.y, "lon": center.x},
             opacity=0.5,
             mapbox_style="open-street-map",
-            zoom=6.5,
+            zoom=5.5,
             labels=self.config.labels,
             hover_data=self.config.hover_data,
             color_discrete_sequence=px.colors.sequential.Plasma
         )
+        hovertempate = '<br>'.join([
+            '<b>County - %{customdata[0]}</b><br>',
+            'US Senate: %{customdata[1]:,}',
+            'US House: %{customdata[2]:,}',
+            'GA Senate: %{customdata[3]:,}',
+            'GA House: %{customdata[4]:,}',
+            'Biden Votes: %{customdata[6]:,}',
+            'Trump Votes: %{customdata[5]:,}',
+            'Undervote: %{customdata[7]:,}',
+            'Estimated Max GOP Votes: %{customdata[8]:,}',
+            '<i>Trump Performance: %{customdata[9]}</i>',
+            'Trump Vote %: %{customdata[10]:.1f}',
+            'Rank: %{customdata[11]}<extra></extra>',
+        ])
+
+        fig.update_traces(
+            customdata=customdata,
+            hovertemplate=hovertempate
+        )
 
         self.add_watermark(fig)
         self.add_legend_text(fig, self.config.description)
-        fig.update_layout(margin={"r": 0, "t": 30, "l": 0, "b": 30})
+        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
         return fig
